9c69e43223f06642652a95a078852e533945b28b
Ashley Jeffs <ash@jeffail.uk>
2019-11-14 15:19:43 +0000

    WIP
    Signed-off-by: Ashley Jeffs <ash@jeffail.uk>

--- src/topology/unit_test.rs
+++ src/topology/unit_test.rs
@@ -27,14 +27,108 @@ pub struct UnitTest {
 
 //------------------------------------------------------------------------------
 
+fn walk(
+    node: &str,
+    inputs: Vec<Event>,
+    transforms: &mut HashMap<String, UnitTestTransform>,
+    aggregated_results: &mut HashMap<String, Vec<Event>>,
+) {
+    let mut results = Vec::new();
+    let mut targets = Vec::new();
+
+    if let Some(target) = transforms.get_mut(node) {
+        for input in inputs {
+            target.transform.transform_into(&mut results, input);
+        }
+        targets = target.next.clone();
+    }
+
+    for child in targets {
+        walk(&child, results.clone(), transforms, aggregated_results);
+    }
+    aggregated_results.insert(node.into(), results);
+}
+
 impl UnitTest {
-    pub fn run(&self) -> Result<(), Vec<String>> {
-        // TODO
-        Ok(())
+    pub fn run(&mut self) -> Vec<String> {
+        let mut errors = Vec::new();
+        let mut results = HashMap::new();
+
+        walk(
+            &self.input.0,
+            vec![self.input.1.clone()],
+            &mut self.transforms,
+            &mut results,
+        );
+
+        for check in &self.checks {
+            if let Some(results) = results.get(&check.extract_from) {
+                for (name, cond) in &check.conditions {
+                    if results.iter().find(|e| cond.check(e)).is_none() {
+                        // TODO
+                    }
+                }
+            } else {
+                errors.push(format!(
+                    "expected resulting events from transform '{}', found none",
+                    check.extract_from
+                ));
+            }
+        }
+
+        errors
     }
 }
 
 //------------------------------------------------------------------------------
+
+fn links_to_a_leaf(
+    target: &str,
+    leaves: &HashMap<String, ()>,
+    link_checked: &mut HashMap<String, bool>,
+    transform_outputs: &HashMap<String, HashMap<String, ()>>,
+) -> bool {
+    if let Some(check) = link_checked.get(target) {
+        return *check;
+    }
+    let linked = leaves.contains_key(target)
+        || if let Some(outputs) = transform_outputs.get(target) {
+            outputs
+                .iter()
+                .filter(|(o, _)| links_to_a_leaf(o, leaves, link_checked, transform_outputs))
+                .count()
+                > 0
+        } else {
+            false
+        };
+    link_checked.insert(target.to_owned(), linked);
+    linked
+}
+
+/// Reduces a collection of transforms into a set that only contains those that
+/// link between our root (test input) and a set of leaves (test outputs).
+fn reduce_transforms(
+    root: &str,
+    leaves: &HashMap<String, ()>,
+    transform_outputs: &mut HashMap<String, HashMap<String, ()>>,
+) {
+    let mut link_checked: HashMap<String, bool> = HashMap::new();
+
+    if !links_to_a_leaf(root, leaves, &mut link_checked, transform_outputs) {
+        transform_outputs.clear();
+    }
+
+    transform_outputs.retain(|name, children| {
+        let linked = name == root || *link_checked.get(name).unwrap_or(&false);
+        if linked {
+            // Also remove all unlinked children.
+            children.retain(|child_name, _| {
+                name == root || *link_checked.get(child_name).unwrap_or(&false)
+            })
+        }
+        linked
+    });
+}
 
 fn build_unit_test(
     definition: &TestDefinition,
@@ -42,13 +136,23 @@ fn build_unit_test(
 ) -> Result<UnitTest, Vec<String>> {
     let mut errors = vec![];
 
-    let mut inputs: HashMap<String, ()> = config
+    // Maps transform names with their output targets (transforms that use it as
+    // an input).
+    let mut transform_outputs: HashMap<String, HashMap<String, ()>> = config
         .transforms
         .iter()
-        .map(|(k, _)| (k.clone(), ()))
+        .map(|(k, _)| (k.clone(), HashMap::new()))
         .collect();
 
-    if !inputs.contains_key(&definition.input.insert_at) {
+    config.transforms.iter().for_each(|(k, t)| {
+        t.inputs.iter().for_each(|i| {
+            if let Some(outputs) = transform_outputs.get_mut(i) {
+                outputs.insert(k.to_string(), ());
+            }
+        })
+    });
+
+    if !transform_outputs.contains_key(&definition.input.insert_at) {
         errors.push(format!(
             "unable to locate test target '{}'",
             definition.input.insert_at,
@@ -56,62 +160,42 @@ fn build_unit_test(
         return Err(errors);
     }
 
-    let mut prev_inputs_len = inputs.len();
+    let mut leaves: HashMap<String, ()> = HashMap::new();
+    definition.outputs.iter().for_each(|o| {
+        leaves.insert(o.extract_from.clone(), ());
+    });
 
-    let mut transforms: Vec<(String, Vec<String>, Box<dyn Transform>)> = config
-        .transforms
-        .iter()
-        .filter_map(|(k, t)| {
-            let transform_inputs: Vec<String> = t
-                .inputs
-                .iter()
-                .cloned()
-                .filter(|i| inputs.contains_key(i))
-                .collect();
+    // Reduce the configured transforms into just the ones connecting our test
+    // target with output targets.
+    reduce_transforms(&definition.input.insert_at, &leaves, &mut transform_outputs);
 
-            if transform_inputs.len() == 0 && *k != definition.input.insert_at {
-                inputs.remove(k);
-                return None;
-            }
-
-            match t.inner.build() {
-                Ok(transform) => Some((k.clone(), transform_inputs, transform)),
+    // Build reduced transforms.
+    let mut transforms: HashMap<String, UnitTestTransform> = HashMap::new();
+    for (name, transform_config) in &config.transforms {
+        if let Some(outputs) = transform_outputs.remove(name) {
+            match transform_config.inner.build() {
+                Ok(transform) => {
+                    transforms.insert(
+                        name.clone(),
+                        UnitTestTransform {
+                            transform: transform,
+                            next: outputs.into_iter().map(|(k, _)| k).collect(),
+                        },
+                    );
+                }
                 Err(err) => {
-                    errors.push(format!("failed to build transform '{}': {}", k, err));
-                    None
+                    errors.push(format!("failed to build transform '{}': {}", name, err));
                 }
             }
-        })
-        .collect();
+        }
+    }
 
     if !errors.is_empty() {
         return Err(errors);
     }
 
-    // Keep reducing our transforms until we have the smallest possible set.
-    while prev_inputs_len > inputs.len() {
-        prev_inputs_len = inputs.len();
-
-        transforms = transforms
-            .into_iter()
-            .filter_map(|(k, t_inputs, transform)| {
-                let transform_inputs: Vec<String> = t_inputs
-                    .into_iter()
-                    .filter(|i| inputs.contains_key(i))
-                    .collect();
-
-                if transform_inputs.len() == 0 && k != definition.input.insert_at {
-                    inputs.remove(&k);
-                    return None;
-                }
-
-                Some((k.clone(), transform_inputs, transform))
-            })
-            .collect();
-    }
-
     definition.outputs.iter().for_each(|o| {
-        if !inputs.contains_key(&o.extract_from) {
+        if !transform_outputs.contains_key(&o.extract_from) {
             errors.push(format!(
                 "unable to complete topology between input target '{}' and '{}'",
                 definition.input.insert_at, o.extract_from
@@ -119,6 +203,7 @@ fn build_unit_test(
         }
     });
 
+    // Build input event.
     // TODO: Support different input event types.
     let input_event = match definition.input.type_str.as_ref() {
         "raw" => match definition.input.value.as_ref() {
@@ -137,6 +222,7 @@ fn build_unit_test(
         }
     };
 
+    // Build all output conditions.
     let checks = definition.outputs.iter().map(|o| {
         let mut conditions: HashMap<String, Box<dyn Condition>> = HashMap::new();
         for (k, cond_conf) in &o.conditions {
@@ -172,7 +258,7 @@ fn build_unit_test(
         Ok(UnitTest {
             name: definition.name.clone(),
             input: (definition.input.insert_at.clone(), input_event),
-            transforms: HashMap::new(), // TODO HashMap<String, UnitTestTransform>,
+            transforms: transforms,
             checks: checks,
         })
     }
@@ -187,7 +273,13 @@ pub fn build_unit_tests(config: &super::Config) -> Result<Vec<UnitTest>, Vec<Str
         .iter()
         .for_each(|test| match build_unit_test(test, config) {
             Ok(t) => tests.push(t),
-            Err(errs) => errors.extend(errs),
+            Err(errs) => {
+                let mut test_err = errs.join("\n");
+                // Indent all line breaks
+                test_err = test_err.replace("\n", "\n\t");
+                test_err.insert_str(0, &format!("Failed to build test '{}':\n", test.name));
+                errors.push(test_err);
+            }
         });
 
     if errors.is_empty() {
